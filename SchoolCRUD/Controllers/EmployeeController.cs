907b82b21a46585c098bccccf97cd7028377abfc
Kamil Żarski <209526@edu.p.lodz.pl>
2019-05-25 11:23:38 +0200

    Add project files.

---
+++ SchoolCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolCRUD.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SchoolCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        // GET: Employee
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+        // GET: Employee/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var emp = await _context.Employees.FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return View(emp);
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
